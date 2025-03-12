42cad2b7a6b36ef01d5bd9611f965fb36fafc90c
djtork3 <asis1989@hotmail.es>
2025-03-12 13:31:05 +0100

    Teleturk

--- addon.js
+++ addon.js
@@ -9,11 +9,11 @@ const seriesData = [
         id: 'una-nueva-vida',
         name: 'Una Nueva Vida',
         poster: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/zhTDZtUfpB8sCbCgjqYKsxfY2os.jpg',
-        description: 'Una Nueva Vida sigue la historia de una joven enfrentándose a los retos del amor y la familia.',
-        genres: ['Drama', 'Romance'],
+        description: 'Halis Korhan, desesperado por enderezar el rumbo de su nieto Ferit, decide casarlo para poner fin a su comportamiento irresponsable. Sin embargo, los planes toman un giro inesperado cuando Ferit se enamora de Seyran. Este romance no solo altera el destino de la pareja, sino también el de toda la familia, desatando conflictos y secretos ocultos.',
+        genres: ['Drama', 'Romance', 'Familia'],
         released: '2023-01-01',
-        cast: ['Actor 1', 'Actor 2'],
-        director: 'Director Name',
+        cast: ['Afra Saraçoğlu', 'Mert Ramazan Demir'],
+        director: 'Burcu Alptekin',
         runtime: '45 min',
         rating: '8.5',
         trailer: 'https://www.youtube.com/watch?v=8442qABMQ1s',
@@ -25,7 +25,7 @@ const seriesData = [
                 episode: 26,
                 magnet: 'magnet:?xt=urn:btih:7c5c6b1ebc256cf3f28a29cda488a3ba173651d1&dn=Una%20nueva%20vida%20%5BHDTV%5D%5BCap.126%5D&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337%2Fannounce&tr=udp%3A%2F%2Fopen.demonii.com%3A1337%2Fannounce&tr=udp%3A%2F%2Fopen.tracker.cl%3A1337%2Fannounce&tr=udp%3A%2F%2Fopen.stealth.si%3A80%2Fannounce&tr=udp%3A%2F%2Ftracker.torrent.eu.org%3A451%2Fannounce&tr=udp%3A%2F%2Fexodus.desync.com%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.theoks.net%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.dump.cl%3A6969%2Fannounce&tr=udp%3A%2F%2Fns-1.x-fins.com%3A6969%2Fannounce&tr=udp%3A%2F%2Fexplodie.org%3A6969%2Fannounce&tr=udp%3A%2F%2Fdiscord.heihachi.pw%3A6969%2Fannounce&tr=http%3A%2F%2Fwww.torrentsnipe.info%3A2701%2Fannounce&tr=http%3A%2F%2Fwww.genesis-sp.org%3A2710%2Fannounce&tr=http%3A%2F%2Ftracker.vanitycore.co%3A6969%2Fannounce&tr=http%3A%2F%2Ftracker.lintk.me%3A2710%2Fannounce&tr=http%3A%2F%2Ftracker.ipv6tracker.org%3A80%2Fannounce&tr=http%3A%2F%2Ftracker.dmcomic.org%3A2710%2Fannounce&tr=http%3A%2F%2Ftracker.bt-hash.com%3A80%2Fannounce&tr=http%3A%2F%2Ft.jaekr.sh%3A6969%2Fannounce&tr=http%3A%2F%2Fhome.yxgz.club%3A6969%2Fannounce&tr=https%3A%2F%2Ftracker.bt4g.com%3A443%2Fannounce',
                 poster: 'https://media.themoviedb.org/t/p/w227_and_h127_bestv2/eoaVuUygFctizRDJezJz3d6mNyP.jpg',
-                description: 'Episodio 26 de la temporada 1 de "Una Nueva Vida".',
+                description: 'Seyran regresa a casa después de ver a Ferit en la casa de Pelin, pero se mantiene firme. Zerrin no pierde ninguna oportunidad de crear la tensión que desea. Los Kazim son invitados de los Korhans en el iftar. Cuando se discute la situación entre Suna y Saffet en la mesa, Ferit obtiene la aprobación de Kazim para tener otra reunión con Saffet en el barco. Mientras el progreso de las cosas es devastador tanto para Suna como para Abidin, Abidin le hace una oferta a Suna. Cuando el nuevo movimiento de Zerrin inquieta a Seyran, toma precauciones de una manera que nadie esperaba.',
                 released: '2025-03-09'
             },
             {
@@ -35,7 +35,7 @@ const seriesData = [
                 episode: 27,
                 magnet: 'magnet:?xt=urn:btih:6001a932ab1f5c0123b7bf231fb50333ad1b08eb&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337%2Fannounce&tr=udp%3A%2F%2Fopen.demonii.com%3A1337%2Fannounce&tr=http%3A%2F%2Fopen.tracker.cl%3A1337%2Fannounce&tr=udp%3A%2F%2Fopen.stealth.si%3A80%2Fannounce&tr=udp%3A%2F%2Ftracker.torrent.eu.org%3A451%2Fannounce&tr=udp%3A%2F%2Fexplodie.org%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.bittor.pw%3A1337%2Fannounce&tr=udp%3A%2F%2Ftracker-udp.gbitt.info%3A80%2Fannounce&tr=udp%3A%2F%2Fretracker01-msk-virt.corbina.net%3A80%2Fannounce&tr=udp%3A%2F%2Fopentracker.io%3A6969%2Fannounce&tr=udp%3A%2F%2Fns-1.x-fins.com%3A6969%2Fannounce&tr=udp%3A%2F%2Fleet-tracker.moe%3A1337%2Fannounce&tr=udp%3A%2F%2Fisk.richardsw.club%3A6969%2Fannounce&tr=udp%3A%2F%2Fexodus.desync.com%3A6969%2Fannounce&tr=http%3A%2F%2Fwww.torrentsnipe.info%3A2701%2Fannounce&tr=http%3A%2F%2Ftracker810.xyz%3A11450%2Fannounce&tr=http%3A%2F%2Ftracker.xiaoduola.xyz%3A6969%2Fannounce&tr=http%3A%2F%2Ftracker.vanitycore.co%3A6969%2Fannounce&tr=http%3A%2F%2Ftracker.moxing.party%3A6969%2Fannounce&tr=http%3A%2F%2Ftracker.lintk.me%3A2710%2Fannounce&tr=https%3A%2F%2Ftracker.bt4g.com%3A443%2Fannounce',
                 poster: 'https://media.themoviedb.org/t/p/w227_and_h127_bestv2/aOvQsM2hy3FKXEQfqUqDQbzQekh.jpg',
-                description: 'Episodio 27 de la temporada 1 de "Una Nueva Vida".',
+                description: 'Para Seyran, las tensiones llegan a su punto límite y ella respira aliviada en la casa de Kazim. Cuando Halis Aga se entera de que Seyran ha dejado la casa, se enfurece mucho. Ifakat se siente satisfecha con este giro de los acontecimientos. Cuando Ferit se entera de la situación con Suna, su reacción es enfrentarse a Abidin, lo que provoca una discusión entre ambos. Mientras el plan de Ifakat avanza sin problemas, la insistencia de Seyran en no regresar a la mansión sigue siendo firme.',
                 released: '2025-03-16'
             }
         ]
@@ -115,7 +115,7 @@ builder.defineMetaHandler((args) => {
             description: episode.description,
             season: episode.season,
             episode: episode.episode,
-            genres: ['Drama', 'Romance'],
+            genres: ['Drama', 'Romance', 'Familia'],
             released: episode.released,
             background: episode.poster,
             logo: episode.poster
